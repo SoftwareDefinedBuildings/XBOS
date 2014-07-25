94150267d275581e3c34c4d23258f2ab94d779e4
Gabe Fierro <gtfierro225@gmail.com>
2014-07-25 16:34:47 -0700

    add binary actuator pressing and continuous actuator pressing

--- openbas/client/openbas.js
+++ openbas/client/openbas.js
@@ -7,12 +7,14 @@ if (Meteor.isClient) {
 
   var getActuators = function(act_uuid) {
     actuatorsDep.depend();
-    return actuators[act_uuid] || {"Actuate": {"Model": "none", "States": [0,1]}};
+    return actuators[act_uuid] || {"Actuator": {"Model": "none", "States": [0,1], "MinValue": 0, "MaxValue": 100}};
   };
 
   var updateActuators = function(act_uuid, data) {
-    actuators[act_uuid] = data;
-    actuatorsDep.changed();
+    if (actuators[act_uuid] !== data) {
+      actuators[act_uuid] = data;
+      actuatorsDep.changed();
+    }
   };
 
   Template.points.pointsAll = function() {
@@ -35,44 +37,97 @@ if (Meteor.isClient) {
   };
 
   Template.actuator_display.type = function() {
-    return getActuators(this.ActuatorUUID).Actuate.Model || "none";
+    return getActuators(this.ActuatorUUID).Actuator.Model || "none";
   };
 
-  // functions to help determine type of actuator based on Actuate.Model from sMAP metadata
+  // functions to help determine type of actuator based on Actuator.Model from sMAP metadata
   Template.actuator_display.helpers({
     isDiscrete: function(template) {
-      return getActuators(this.ActuatorUUID).Actuate.Model === "discrete";
+      return getActuators(this.ActuatorUUID).Actuator.Model === "discrete";
     },
     isBinary: function(template) {
-      return getActuators(this.ActuatorUUID).Actuate.Model === "binary";
+      return getActuators(this.ActuatorUUID).Actuator.Model === "binary";
     },
     isContinuous: function(template) {
-      return getActuators(this.ActuatorUUID).Actuate.Model === "continuous";
+      return getActuators(this.ActuatorUUID).Actuator.Model === "continuous";
     }
   });
 
   Template.actuator_continuous.rendered = function() {
     if (Meteor.isClient) {
-      that = getActuators(this.data.ActuatorUUID);
+      var that = getActuators(this.data.ActuatorUUID);
+      var port = this.data.ServerPort;
       $("#"+this.data.ActuatorUUID).slider({
-        min: that.Actuate.States[0],
-        max: that.Actuate.States[1],
+        min: 0,//that.Actuator.MinValue,
+        max: 50,//that.Actuator.MaxValue,
         value: this.data.value
+      }).on('slideStop', function(e) {
+        console.log("slider", e);
+        console.log(that);
+        Meteor.call("actuate", port, that.Path, e.value, function(err, res) {
+          if (err) {
+            console.log("ERROR", err);
+          }
+          console.log("RES",res);
+        });
       });
     }
   };
 
-  Template.actuator_binary.events({
-    'click a': function () {
-      console.log(this);
-      if (this.value === 1) {
-        this.value = 0;
+  Template.actuator_continuous.value = function() {
+    if (Meteor.isClient) {
+      var p = Points.find({'_id': this._id}).fetch();
+      $('#'+this.ActuatorUUID).slider('setValue', p[0].value);
+      return p[0].value;
+    }
+  };
+
+  Template.actuator_discrete.rendered = function() {
+    if (Meteor.isClient) {
+      that = getActuators(this.data.ActuatorUUID);
+    }
+  };
+
+  Template.actuator_binary.rendered = function() {
+    if (Meteor.isClient) {
+      var that = getActuators(this.data.ActuatorUUID);
+    }
+  };
+
+  Template.actuator_binary.value = function() {
+    if (Meteor.isClient) {
+      var p = Points.find({'_id': this._id}).fetch();
+      if (p[0].value === 0) {
         $('#'+this.ActuatorUUID).removeClass("pressed");
       } else {
-        this.value = 1;
         $('#'+this.ActuatorUUID).addClass("pressed");
       }
-      console.log("clicked a button", this);
+      return p[0].value;
+    }
+  };
+
+
+  Template.actuator_binary.events({
+    'click a': function () {
+      if (this.value === 1) {
+        $('#'+this.ActuatorUUID).removeClass("pressed");
+        var act = getActuators(this.ActuatorUUID);
+        Meteor.call("actuate", this.ServerPort, act.Path, "0", function(err, res) {
+          if (err) {
+            console.log("ERROR", err);
+          }
+          console.log("RES",res);
+        });
+      } else {
+        var act = getActuators(this.ActuatorUUID);
+        Meteor.call("actuate", this.ServerPort, act.Path, "1", function(err, res) {
+          if (err) {
+            console.log("ERROR", err);
+          }
+          console.log("RES",res);
+        });
+        $('#'+this.ActuatorUUID).addClass("pressed");
+      }
     }
   });
 
