258878a3a2ba4c151e28888d549bda58551e583d
David Culler <culler@berkeley.edu>
2014-08-07 08:20:44 -0700

    Added schedule and status page, adjusted nav bar

--- openbas/lib/router.js
+++ openbas/lib/router.js
@@ -1,17 +1,13 @@
 Router.map(function() {
-    this.route('home', {path: '/'});
-
-    this.route('about');
-
+    this.route('dashboard', {path: '/'});
+    this.route('schedule');
+    this.route('status');
     this.route('points');
-
     this.route('actuators');
-
     this.route('lighting');
-
     this.route('hvac');
-
-    this.route('dashboard');
+    this.route('about');
+    this.route('test');
 
     this.route('pointDetail', {
       path: '/points/:uuid',
