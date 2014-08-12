d04c84e822ba9f48fdc16e03eba9105aaa969d26
Gabe Fierro <gtfierro225@gmail.com>
2014-08-12 12:06:59 -0700

    add some comments

--- openbas/client/status.js
+++ openbas/client/status.js
@@ -1,6 +1,12 @@
 if (Meteor.isClient) {
 
   Template.status.sources = function () {
+    /*
+     * Grabs all the HVAC, Lighting and Monitoring points. These 
+     * have been configured, so we can read their metadata. Thanks
+     * to the 'querysystem' function, thse are pre-aggregated by
+     * their driver path instead of the individual timeseries
+     */
     var sources = [];
     sources.push.apply(sources, HVAC.find().fetch());
     sources.push.apply(sources, Lighting.find().fetch());
@@ -9,6 +15,10 @@ if (Meteor.isClient) {
   };
 
   Template.device.driverPath = function() {
+    /*
+     * From the driver module path (e.g. smap.drivers.lights.VirtualLightDriver),
+     * we generate the URL to the code of the driver
+     */
     var baseurl = "https://github.com/SoftwareDefinedBuildings/smap/tree/unitoftime/python";
     var components = this.driver.split('.');
     _.each(components, function(val, idx) {
