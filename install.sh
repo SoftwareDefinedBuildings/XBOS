657fa23c001657cb93ccbcfa426594af4dbb577c
Gabe Fierro <gtfierro225@gmail.com>
2014-08-27 22:31:42 -0700

    update install command

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # This is the install script for OpenBAS
 
-set -e
-set -u
+function notify() { 
+sleep 1
+msg=$1
+length=$((${#msg}+4))
+buf=$(printf "%-${length}s" "#")
+echo ${buf// /#}
+echo "  "$msg
+echo ${buf// /#}
+sleep 2 ;}
 
 # display on stderr
-#exec 1>&2
+exec 1>&2
 
 UNAME=$(uname)
 if [ "$UNAME" != "Linux" ] ; then
@@ -21,19 +28,24 @@ if [ "$ISUBUNTU" != "Ubuntu" -o "$UBUNTUVERSION" != "14.04" ] ; then
     exit 1
 fi
 
-sudo apt-get install -y expect
+notify "Installing APT packages..."
+`sudo apt-get install -y expect mongodb npm libssl-dev git-core pkg-config build-essential gcc g++`
 
-echo "Adding cal-sdb package repository..."
+notify "Adding cal-sdb package repository..."
 sudo add-apt-repository ppa:cal-sdb/smap
 
-echo "Updating to receive packages from cal-sdb..."
+notify "Updating to receive packages from cal-sdb..."
 sudo apt-get update
 
-echo "Installing npm..."
-sudo apt-get install -y npm
+notify "Installing NVM and latest NodeJS..."
+git clone git://github.com/creationix/nvm.git $HOME/nvm
+source $HOME/nvm/nvm.sh
+latestnode=$(nvm ls-remote | tail -n 1)
+nvm install $latestnode
+nvm use $latestnode
 
-echo "Installing sMAP and sMAP dependencies..."
-sudo apt-get install -y python-smap readingdb
+notify "Installing sMAP and sMAP dependencies..."
+`sudo apt-get install -y python-smap readingdb`
 
 expect -c "
 spawn sudo apt-get install -y powerdb2
@@ -53,14 +65,25 @@ expect {
 }
 "
 
+notify "Downloading OpenBAS..."
 curl -O http://54.183.169.17/openbas.tgz
 tar xzf openbas.tgz
 
-#echo "Installing Meteor..."
-#curl https://install.meteor.com | sh
-#
-#echo "Adding Meteor's Node to PATH..."
-#export PATH=~/.meteor/tools/latest/bin:$PATH
-#
-#echo "Installing meteorite..."
-#sudo -H npm install -g meteorite
+sudo mkdir -p /data/mongodb
+cat <<EOF >> /etc/supervisor/conf.d/openbas.conf
+[program:openbas]
+command = /usr/bin/nodejs main.js
+directory = /home/oski/bundle
+priority = 2
+autorestart = true
+environment = PORT=3000, MONGO_URL=mongodb://localhost:27017/meteor
+stdout_logfile = /var/log/openbas.stdout.log
+stdout_logfile_maxbytes = 50MB
+stdout_logfile_backups = 5
+stderr_logfile = /var/log/openbas.stderr.log
+stderr_logfile_maxbytes = 50MB
+stderr_logfile_backups = 5
+EOF
+
+sudo dpkg --configure -a
+
