44d82887ef6e47890c433753c677352a1522450c
Michael Andersen <michael@steelcode.com>
2014-08-29 16:59:52 -0700

    Tweak maketarball

--- maketarball.sh
+++ maketarball.sh
@@ -1,7 +1,13 @@
 echo "[]" > openbas/private/testrooms.json
 cd openbas
-mrt reset
+which mrt
+if [ $? != 0 ]
+then
+  echo "No MRT, assuming clean repository"
+else
+  mrt reset
+fi
 cd ..
-rm openbas.tgz
+rm -f openbas.tgz
 tar zcf openbas.tgz openbas upmu-plotter
 git checkout openbas/private
