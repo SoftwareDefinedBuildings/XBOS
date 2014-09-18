c6b5f63288a4de7343d1f3b1d6cedfbb61d3058e
Gabe Fierro <gtfierro225@gmail.com>
2014-09-17 20:20:32 -0700

    moar comments

--- zone-controller-tutorial/zone1controller.py
+++ zone-controller-tutorial/zone1controller.py
@@ -3,8 +3,11 @@ from smap.services.zonecontroller import ZoneController
 class AvgSensorFollowMaster(ZoneController):
     def setup(self, opts):
         ZoneController.setup(self, opts)
+        # we want to publish the heat/cool setpoints
         self.add_timeseries('/temp_heat','F',data_type='double')
         self.add_timeseries('/temp_cool','F',data_type='double')
+        # we want to write a special averaging function to handle the temperature sensors, so
+        # we use add_callback to run it automatically when new sensor data comes in
         self.add_callback('temp_sensor', self.avg_temp, opts.get('subscribe/temp_sensor'))
 
     def step(self):
@@ -17,4 +20,5 @@ class AvgSensorFollowMaster(ZoneController):
         self.add('/temp_cool', self.points['temp_cool'])
 
     def avg_temp(self, point, uuids, data):
+        # averages the readings
         self.points['avg_temp'] = sum(map(lambda x: x[-1][1], data)) / float(len(data))
