c15cc24baf72fe29eb7b47bc4c20f7a54603f379
Gabe Fierro <gtfierro225@gmail.com>
2014-09-29 15:47:15 -0700

    add rtu_load plotter

---
+++ monitor/README.md
@@ -0,0 +1,1 @@
+This is a collection of scripts and apps built over the sMAP archiver to work alongside OpenBAS.
---
+++ monitor/rtu_load.py
@@ -0,0 +1,43 @@
+from smap.archiver.client import SmapClient
+import time
+import pandas as pd
+#pd.options.display.mpl_style = 'default'
+
+client = SmapClient('http://ciee.cal-sdb.org:8079')
+# timestamps
+end = int(time.time())
+start = end - 60*60*24*30 # last month
+print start, end
+
+# get energy data for same timeframe
+res = client.query('select uuid where Metadata/System = "Monitoring" and Properties/UnitofMeasure = "kW"')
+uuids = [x['uuid'] for x in res]
+data = dict(zip(uuids,client.data_uuid(uuids, start, end, cache=False)))
+
+# create dataframe, use time as index
+demand = pd.DataFrame(data.values()[0])
+demand[0] = pd.to_datetime(demand[0], unit='ms')
+demand.index = demand[0]
+del demand[0]
+
+# get all hvac_state timeseries
+res = client.query('select uuid where Metadata/System = "HVAC" and Properties/UnitofMeasure = "Mode" and Path like "%hvac_state"')
+uuids = [x['uuid'] for x in res]
+data = dict(zip(uuids,client.data_uuid(uuids, start, end, cache=False)))
+
+for uuid in data.iterkeys():
+    hvac = pd.DataFrame(data[uuid])
+    hvac[0] = pd.to_datetime(hvac[0], unit='ms')
+    hvac.index = hvac[0]
+    del hvac[0]
+    zone = client.query("select Metadata/HVACZone where uuid = '{0}'".format(uuid))[0]['Metadata']['HVACZone']
+
+    # resample every minute
+    hvac_rs = hvac.resample('10S',pd.np.max)
+    demand_rs = demand.resample('10S',pd.np.max)
+    # join on the timestamps to filter out missing data
+    merge = hvac_rs.merge(demand_rs, left_index=True, right_index=True)
+    merge.columns = ['state','demand']
+    print len(merge)
+    merge.plot(figsize=(30,10)).get_figure().savefig('{0}.png'.format(zone))
+
