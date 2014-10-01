159fb96573c31ca0f85e4b1b90b8b6f5fe61ae11
Gabe Fierro <gtfierro225@gmail.com>
2014-09-30 21:03:36 -0700

    cannot have "_" in id names for d3

--- monitor/report/app.py
+++ monitor/report/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, url_for, jsonify
 import json
 
+def fix_name(name):
+    return name.replace(" ","_")
+
 data = json.load(open('report.json'))
 app = Flask(__name__)
+app.add_template_global(fix_name)
 
 @app.route("/")
 def report():
@@ -13,6 +17,18 @@ def report():
 @app.route("/demanddata")
 def demanddata():
     return jsonify({'data': [{'date': k,'value': v} for k,v in data['demand']['Demand Data'].iteritems()]})
+
+@app.route("/zonedata/<key>/<zone>")
+def zonedata(key, zone):
+    """
+    key: "Min Daily Avg Demand" from report.demand_report
+    zone: zone name like "DOSA"
+    """
+    key = key.replace("_"," ")
+    ret = {}
+    for ts, dd in data['demand'][key]['Data'][zone].iteritems():
+        ret[ts] = [{'date': k, 'value': v} for k,v in dd.iteritems()]
+    return jsonify(ret)
 
 if __name__=='__main__':
     app.run(debug=True)
