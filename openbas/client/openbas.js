f9fa6ab114fa255fc59089a809b5a1228f833a85
Tyler Hoyt <tyler.hoyt@gmail.com>
2014-10-13 16:38:57 -0700

    Format decimals in points table

--- openbas/client/openbas.js
+++ openbas/client/openbas.js
@@ -19,6 +19,15 @@ Template.points.helpers({
   }
 });
 
+Template.point_row.value_fmt = function() {
+  var val = this.value
+  if ((String(val).split('.')[1] || []).length > 2){
+    return this.value.toFixed(2);
+  } else {
+    return this.value;
+  } 
+}
+
 Template.navbar.building_name = function() {
    return Meteor.settings.public.building_name;
 };
