20079c5a3d52fa2ae35635e87377cb82e937ba83
Tyler Hoyt <tyler.hoyt@gmail.com>
2014-10-22 16:35:28 -0700

    Make the router scroll to the top when you switch pages

--- openbas/lib/router.js
+++ openbas/lib/router.js
@@ -122,3 +122,17 @@ Router.map(function() {
     });
 
 });
+
+Router._filters = {
+  resetScroll: function () {
+    var scrollTo = window.currentScroll || 0;
+    $('body').scrollTop(scrollTo);
+    $('body').css("min-height", 0);
+  }
+};
+
+var filters = Router._filters;
+
+if(Meteor.isClient) {
+  Router.onAfterAction(filters.resetScroll); // for all pages
+}
