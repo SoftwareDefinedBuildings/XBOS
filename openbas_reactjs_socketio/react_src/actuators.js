3052fa37c6487815174d1fb8eeb41477f2ec24ea
Gabe Fierro <gtfierro225@gmail.com>
2015-03-06 13:56:33 -0800

    try an implementation of ContinuousActuator

--- openbas_reactjs_socketio/react_src/actuators.js
+++ openbas_reactjs_socketio/react_src/actuators.js
@@ -1,45 +1,38 @@
-/*
- * This will render a slider bar between a range with a given step interpolating between
- * the low and high ends of the range. Sliding the knob will result in a POST request being
- * performed in the backend. The required props are
- *
- * @low: number identifying the lower bound of the range
- * @high: number identifying the higher bound of the range
- * @step: the unit of interpolation between @low and @high
- * @url: the URL that will be POSTed to when the slider changes
- * @prop: the current value of the slider -- this should come from the parent element, so this is immutable
- *        for this component
- *
- * Accepted state is
- *
- * @data: a JSON object to be sent on the POST request. If empty, then this
- * will not be sent along
- */
 var ContinuousActuator = React.createClass({
+    getInitialState: function() {
+        return({loading: false});
+    },
+    handleSubmit: function(event) {
+        event.preventDefault();
+        this.setState({loading: true});
+        var value = parseFloat(this.refs.value.getDOMNode().value.trim());
+        if (!isNaN(value)) {
+            console.log("new value", value);
+            var req = {uuid: this.props.uuid, request: value};
+            console.log(req);
+            this.socket.emit('actuate', req);
+        }
+        setTimeout(function() {
+            this.setState({loading: false});
+            this.refs.value.getDOMNode().value = '';
+        }.bind(this), 2000);
+    },
+    componentWillMount: function() {
+        this.socket = io.connect();
+    },
     render: function() {
         return(
             <div className="continuousActuator">
-                <p>Slider</p>
+                <form onSubmit={this.handleSubmit} >
+                  <input type="text" ref="value" />
+                  { !this.state.loading ?  <input type="submit" value="Override" /> : <label>Loading...</label> }
+                </form>
             </div>
         );
     }
 });
 
-/*
- * This will render two buttons, one for "on" and one for "off". Clicking a button
- * will result in a POST request being performed. the required props are
- *
- * @onUrl: URL that is POSTed when the "on" button is pushed
- * @onLabel: label for the "on" button
- * @onData: data for the POST request when "on" is pushed
- *
- * @offUrl: URL that is POSTed when the "off" button is pushed
- * @offLabel: label for the "off" button
- * @offData: data for the POST request when "off" is pushed
- *
- * State:
- * @on: if @on is 1, then the "on" will be depressed (the bootstrap "active" prop), else the "off" button will be depressed
- */
+
 var BinaryActuator = React.createClass({
     getInitialState: function() {
         return({on: false,
