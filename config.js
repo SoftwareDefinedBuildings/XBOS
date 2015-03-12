f70e5bd219dfad57334433dd33f6181610c8e04a
Gabe Fierro <gtfierro225@gmail.com>
2015-03-12 14:34:21 -0700

    add config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.apikey = "ADM5fw0z1HdbMCDZ6JSQAveuZyKDc_GicOsKBqzznpySgiUrmLENUGcqF69PXhXFgXOQ_GS98TUt0O-8SHXgKg==";
+config.uuid = "15fbd44c-c3c4-11e4-95aa-0cc47a0f7eea";
+config.httpArchiverHost = 'localhost';
+config.httpArchiverPort = 8079;
+config.httpArchiverUrl = 'http://'+config.httpArchiverHost+":"+config.httpArchiverPort;
+config.wsArchiverHost = 'localhost';
+config.wsArchiverPort = 8078;
+config.wsArchiverUrl = 'ws://'+config.wsArchiverHost+":"+config.wsArchiverPort;
+
+module.exports = config;
+
