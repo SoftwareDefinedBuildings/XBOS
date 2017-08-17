2c3d85f2b8ee0d26e37cb7bbda4557900855fc83
Gabe Fierro <gtfierro225@gmail.com>
2017-08-16 19:29:29 -0700

    generalize demand response app

--- apps/demand_response/app.py
+++ apps/demand_response/app.py
@@ -1,3 +1,4 @@
+import json
 import time
 from Queue import Queue
 from threading import Thread
@@ -7,8 +8,15 @@ from xbos.services.hod import HodClient
 from xbos.devices.thermostat import Thermostat
 from xbos.devices.light import Light
 
+with open("params.json") as f:
+    try:
+        params = json.loads(f.read())
+    except ValueError:
+        print "Invalid parameter file"
+        sys.exit(1)
+
 c = get_client()
-hod = HodClient("ciee/hod",c)
+hod = HodClient(params["HOD_URI"],c)
 
 lighting_query = """SELECT ?equipment ?uri WHERE {
 ?equipment rdf:type/rdfs:subClassOf* brick:Lighting_System .
@@ -20,47 +28,12 @@ tstat_query = """SELECT ?equipment ?uri WHERE {
 ?equipment bf:uri ?uri .
 } LIMIT 500;"""
 
-def light_strategy(self):
-    self.old_brightness = self.brightness
-    print "old", self.old_brightness
-    new_brightness = 25
-    print "change to", new_brightness
-    #self.write({'brightness': new_brightness})
-
-def light_reset(self):
-    print "reset to", self.old_brightness
-    #self.write({'brightness': self.old_brightness})
-
-
 results = hod.do_query(lighting_query)
 lights = []
 if results["Count"] > 0:
     for row in results["Rows"]:
         l = Light(c, row["?uri"])
         lights.append(l)
-
-def tstat_strategy(self):
-    print self._state
-    self.old_hsp = self.heating_setpoint
-    self.old_csp = self.cooling_setpoint
-    print "old",self.old_hsp,self.old_csp
-    new_hsp = self.old_hsp-2
-    new_csp = self.old_csp+2
-    print "change to", new_hsp,new_csp
-    self.write({
-        'heating_setpoint': new_hsp,
-        'cooling_setpoint': new_csp,
-        'override': True
-    })
-    print "done writing"
-
-def tstat_reset(self):
-    print "reset to",self.old_hsp,self.old_csp
-    self.write({
-        'heating_setpoint': self.old_hsp,
-        'cooling_setpoint': self.old_csp,
-        'override': True
-    })
 
 results = hod.do_query(tstat_query)
 tstats = []
@@ -69,18 +42,46 @@ if results["Count"] > 0:
         t = Thermostat(c, row["?uri"])
         tstats.append(t)
 
+#### Light Strategy: dim to 25%
+def light_strategy(self):
+    self.old_brightness = self.brightness
+    new_brightness = 25
+    self.write({state: self.state, 'brightness': new_brightness})
+
+def light_reset(self):
+    self.write({state: self.state, 'brightness': self.old_brightness})
+
+
+#### Thermostat Strategy: widen deadband by 8 degrees
+def tstat_strategy(self):
+    self.old_hsp = self.heating_setpoint
+    self.old_csp = self.cooling_setpoint
+    new_hsp = self.old_hsp-4
+    new_csp = self.old_csp+4
+    self.write({
+        'heating_setpoint': new_hsp,
+        'cooling_setpoint': new_csp,
+        'override': True
+    })
+
+def tstat_reset(self):
+    self.write({
+        'heating_setpoint': self.old_hsp,
+        'cooling_setpoint': self.old_csp,
+        'override': True
+    })
+
+
+### Plumbing for reacting to events
 dr_ponum = (2,0,0,1)
 dr_queue = Queue()
-
 def on_dr_event(bw_message):
     dr_queue.put(bw_message)
-
 def do_dr_event(bw_message):
     for po in bw_message.payload_objects:
         if po.type_dotted == dr_ponum:
             event = msgpack.unpackb(po.content)
             duration = int(event["duration"])
-            print "GOT DR EVENT for",duration,"seconds"
     for tstat in tstats:
         tstat_strategy(tstat)
     for light in lights:
@@ -92,7 +93,6 @@ def do_dr_event(bw_message):
         tstat_reset(tstat)
     for light in lights:
         light_reset(light)
-
 def dr_handler():
     while True:
         item = dr_queue.get()
@@ -100,7 +100,7 @@ def dr_handler():
         dr_queue.task_done()
 
 time.sleep(10)
-c.subscribe("ciee/demand_response", on_dr_event)
+c.subscribe(params["DR_URI"], on_dr_event)
 print "Waiting for Demand Response events"
 
 dr_worker = Thread(target=dr_handler)
