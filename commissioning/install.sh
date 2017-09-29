eb88da3873bf65f358ff7ade95656b69fd9021c6
Gabe Fierro <gtfierro225@gmail.com>
2017-09-29 09:46:26 -0700

    modularize the install script a bit

--- commissioning/install.sh
+++ commissioning/install.sh
@@ -67,6 +67,209 @@ sleep_til_valid() {
     done
 }
 
+check_variables() {
+    if [ -z "$BW2_DEFAULT_CONTACT" ]; then
+        $echo "${ERROR}You need to provide a default contact \$BW2_DEFAULT_CONTACT ${NC}"
+        exit 1
+    fi
+
+    if $CONFIGURE_NAMESPACE ; then
+        if [ -z "$NAMESPACE_ALIAS" ]; then
+            $echo "${ERROR}You need to provide a namespace alias \$NAMESPACE_ALIAS ${NC}"
+            exit 1
+        fi
+        if [ -z "$DESIGNATED_ROUTER_VK" ]; then
+            $echo "${ERROR}You need to provide a DR VK \$DESIGNATED_ROUTER_VK ${NC}"
+            exit 1
+        fi
+    fi
+}
+
+install_dependencies() {
+    # install dependencies
+    $echo "${INFO}Updating apt repos and installing dependencies${NC}"
+
+    $sh_c 'apt-get update >/dev/null'
+    $sh_c 'apt-get install -y git python2.7 python-pip python-dev curl bc docker.io libssl-dev htop nmon'
+}
+
+install_bw2() {
+    # check if bosswave is installed, but update it either way
+    if command_exists bw2; then
+        $echo "${INFO}BOSSWAVE already installed! Updating...${NC}"
+    else
+        $echo "${INFO}Installing Bosswave${NC}"
+    fi
+
+    $sh_c $(curl get.bw2.io/agent | bash)
+    sleep 5
+    $sh_c "$start_bw2"
+    $echo "${INFO}Wait for agent to restart${NC}"
+    sleep 20
+    # xargs trick to remove surrounding whitespace
+    current_block=$(bw2 status  | sed -n -e 's/Current block: \(.*\).*/\1/p' | xargs)
+    seen_block=$(bw2 status  | sed -n -e 's/Seen block: \(.*\).*/\1/p' | xargs)
+    diff=$(($seen_block - $current_block))
+    waited=0
+    $echo "${INFO}Wait for agent catch up on chain${NC}"
+    while [ $diff -ne 0 ] || [ $seen_block -eq 0 ]; do
+        current_block=$(bw2 status  | sed -n -e 's/Current block: \(.*\).*/\1/p' | xargs)
+        seen_block=$(bw2 status  | sed -n -e 's/Seen block: \(.*\).*/\1/p' | xargs)
+        if [ $seen_block -ne 0 ]; then
+            diff=$(($seen_block - $current_block))
+            pct=$(bc <<< "scale=2; 100*$current_block/$seen_block")
+            human_waited=$(date -u -d @${waited} +"%T")
+            printf "\r$current_block/$seen_block ($pct%% done). Waited $human_waited so far..."
+        else
+            printf "\rWaiting for peers to send blocks..."
+        fi
+        sleep 1
+        waited=$(($waited + 1))
+    done
+    $echo "${OK}Installed BOSSWAVE${NC}"
+
+    if $CONFIGURE_NAMESPACE ; then
+        # configure BW2_AGENT
+        $echo "${INFO}Rewriting BW2_AGENT to 172.17.0.1:28589 for docker${NC}"
+        $sh_c "sed -i -e 's/ListenOn=127.0.0.1:28589/ListenOn=172.17.0.1:28589/' /etc/bw2/bw2.ini"
+        export BW2_AGENT="172.17.0.1:28589"
+        $echo "${INFO}restarting...${NC}"
+        $sh_c "$reload_bw2"
+        sleep 10
+    else
+        export BW2_AGENT="127.0.0.1:28589"
+    fi
+    $echo "${OK}Reconfigured BOSSWAVE${NC}"
+}
+
+configure_entities() {
+    # configure BW2_DEFAULT_ENTITY
+    bw2 inspect $BW2_DEFAULT_ENTITY > /dev/null 2>&1
+    if [ -z "$BW2_DEFAULT_ENTITY" ] || [ $? -ne 0 ]; then
+        if [ ! -f defaultentity.ent ]; then
+            $echo "${INFO}Could not find BW2_DEFAULT_ENTITY. Creating defaultentity.ent${NC}"
+            bw2 mke -o defaultentity.ent -e 100y -m "Administrative key" -n
+            # sleep_til_valid defaultentity.ent
+        fi
+        export BW2_DEFAULT_ENTITY="$(pwd)/defaultentity.ent"
+    fi
+
+    bw2 inspect $BW2_DEFAULT_BANKROLL > /dev/null 2>&1
+    if [ -z "$BW2_DEFAULT_BANKROLL" ] || [ $? -ne 0 ]; then
+        $echo "${INFO}Could not find BW2_DEFAULT_BANKROLL. Setting it to ${BW2_DEFAULT_ENTITY} ${NC}"
+        export BW2_DEFAULT_BANKROLL="$BW2_DEFAULT_ENTITY"
+    fi
+    git add defaultentity.ent
+    git commit -m 'Added default entity and bankroll'
+
+    # get the monEH
+    address=$(bw2 i $BW2_DEFAULT_BANKROLL | sed -n 's/.* 0 (\(0x.*\)) .*/\1/p')
+    funds=$(bw2 i $BW2_DEFAULT_BANKROLL | sed -n 's/.* 0 (0x.*) \([0-9]*\).* .*/\1/p')
+    if [[ "$funds" -lt 500 ]] ; then
+        $echo "${INFO}You only have $funds Ξ, but we recommend 500 Ξ. ${NC}"
+        $echo "${INFO}Ask someone to send $((500 - $funds)) Ξ to address $address . We'll wait${NC}"
+        confirmY "${PROMPT}Do you have the funds? If you don't, we will try our best, but some commands may fail. Continue? [Y/n]${NC}"
+        if [ $? -ne 0 ]; then exit ;fi
+    fi
+
+    # publish default entity
+    bw2 i $BW2_DEFAULT_ENTITY -p
+    $echo "${OK}Now have \$BW2_DEFAULT_ENTITY, \$BW2_DEFAULT_BANKROLL${NC}"
+}
+
+configure_namespace() {
+    $echo "${INFO}Setting up namespace${NC}"
+    # setup namespace entity
+
+    # copy over existing entity if configured
+    if [ ! -z "$NAMESPACE_ENTITY" ]; then
+        $sh_c "cp $NAMESPACE_ENTITY namespace.ent"
+    fi
+
+    # setup entity if it doesn't exist
+    if [ ! -f "namespace.ent" ]; then
+        $echo "${INFO}Creating a namespace entity${NC}"
+        bw2 mke -o namespace.ent -e 100y -m "Namespace entity"
+        sleep_til_valid namespace.ent
+    fi
+
+    set -e
+    nsvk=$(bw2 i namespace.ent | sed -n 's/.*Entity VK: \(.*\).*/\1/p')
+
+    # setup alias if its null
+    export EXISTING_ALIAS=$(bw2 i namespace.ent | sed -n 's/.*Alias: \(.*\).*/\1/p')
+    if [ "$EXISTING_ALIAS" != "$NAMESPACE_ALIAS" ] && [ ! -z "$NAMESPACE_ALIAS" ] && [ ! -z "$nsvk" ] ; then
+        $echo "${INFO}Creating namespace alias${NC}"
+        bw2 mkalias --long "$NAMESPACE_ALIAS" --b64 "$nsvk"
+    else
+        if [ -z "$NAMESPACE_ALIAS" ]; then
+            $echo "${ERROR}No alias configured, using VK ${NC}"
+            export NAMESPACE_ALIAS=$nsvk
+        fi
+        $echo "${OK}Already have alias $NAMESPACE_ALIAS${NC}"
+    fi
+    set +e
+
+    # confirm admin privileges on namespace for defaultentity.ent
+    bw2 bc -u "$NAMESPACE_ALIAS/*"
+    if [ $? -ne 0 ]; then
+        $echo "${INFO}Make DOT on $NAMESPACE_ALIAS/*${NC}"
+        set -e
+        bw2 mkdot -f namespace.ent -t $BW2_DEFAULT_ENTITY -u "$NAMESPACE_ALIAS/*" -m "Admin access to $NAMESPACE_ALIAS" --ttl 10
+        set +e
+    fi
+
+    # setup designated router
+    if [ ! -z "$DESIGNATED_ROUTER_VK" ]; then
+        $echo "${INFO}Now we need to set up the designated router (DR) for this namespace. Ask someone who runs a DR to run the following:${NC}"
+        $echo "${INFO}bw2 mkdroffer --dr /etc/bw2/router.ent --ns $NAMESPACE_ALIAS${NC}"
+        #$echo "${INFO}Now type in the VK of the DR router entity (obtained by bw2 lsdro --ns $NAMESPACE_ALIAS, or by asking)${NC}"
+        bw2 adro --dr $DESIGNATED_ROUTER_VK --ns namespace.ent
+    fi
+    $echo "${GO}Namespace configured${NC}"
+    git add namespace.ent
+    git commit -m 'Configured namespace'
+}
+
+install_go() {
+    $echo "${YELLOW}Installing Go${NC}"
+    $sh_c "curl -O https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz"
+    $sh_c "tar -C /usr/local -xzf go1.9.linux-amd64.tar.gz"
+    export PATH=/usr/local/go/bin:$PATH
+    export PATH=$(go env GOPATH):$PATH
+    $echo "${OK}Installed go${NC}"
+}
+
+install_spawnd() {
+    # create spawnpoint entity
+    $echo "${YELLOW}Installing spawnpoint${NC}"
+
+    # copy spawnd entity over if configured
+    if [ ! -z "$SPAWND_ENTITY" ]; then
+        $sh_c "cp $SPAWND_ENTITY spawnpoint.ent"
+    fi
+
+    # or create it if it doesn't exist
+    if [ ! -f spawnpoint.ent ]; then
+        set -e
+        $echo "${INFO}Could not find spawnpoint.ent. Creating...${NC}"
+        bw2 mke -o spawnpoint.ent -e 100y -m "Spawnpoint entity"
+        sleep_til_valid spawnpoint.ent
+        $echo "${INFO}Updating permissions for spawnpoint.ent${NC}"
+        bw2 mkdot -f namespace.ent -t spawnpoint.ent -u "$NAMESPACE_ALIAS/sp/*" -m "Spawnpoint access"
+        set +e
+    fi
+
+    # install spawnpoint server
+    $sh_c "cp spawnpoint.ent /etc/spawnd/spawnpooint.ent"
+    export SPAWND_INSTALLER_ENTITY=spawnpoint.ent
+    export SPAWND_INSTALLER_PATH="$NAMESPACE_ALIAS/sp"
+    export SPAWND_INSTALLER_MEM_ALLOC="$SPAWND_MEM_ALLOC"
+    export SPAWND_INSTALLER_CPU_SHARES="$SPAWND_CPU_SHARES"
+    $sh_c $(curl get.bw2.io/spawnpoint | bash)
+    git add spawnpoint.ent
+    git commit -m 'Configured spawnpoint'
+}
 
 do_install() {
     cat >&2 <<'EOF'
@@ -133,11 +336,7 @@ EOF
         reload_bw2="systemctl daemon-reload -q --no-pager > /dev/null 2>&1 && systemctl restart -q --no-pager bw2.service > /dev/null 2>&1"
     fi
 
-    # install dependencies
-    $echo "${INFO}Updating apt repos and installing dependencies${NC}"
-
-    $sh_c 'apt-get update >/dev/null'
-    $sh_c 'apt-get install -y git python2.7 python-pip python-dev curl bc docker.io libssl-dev htop nmon'
+    install_dependencies
 
     # trick to get directory of executing script, and move there
     dot="$(cd "$(dirname "$0")"; pwd)"
@@ -145,6 +344,8 @@ EOF
 
     # import config
     source config.sh
+
+    check_variables
 
 
     # set up local git repository
@@ -154,180 +355,21 @@ EOF
     git add config.sh
     git commit -m 'added config file'
 
-    # check if bosswave is installed, but update it either way
-    if command_exists bw2; then
-        $echo "${INFO}BOSSWAVE already installed! Updating...${NC}"
-    else
-        $echo "${INFO}Installing Bosswave${NC}"
-    fi
+    install_bw2
 
-    $sh_c $(curl get.bw2.io/agent | bash)
-    sleep 5
-    $sh_c "$start_bw2"
-    $echo "${INFO}Wait for agent to restart${NC}"
-    sleep 20
-    # xargs trick to remove surrounding whitespace
-    current_block=$(bw2 status  | sed -n -e 's/Current block: \(.*\).*/\1/p' | xargs)
-    seen_block=$(bw2 status  | sed -n -e 's/Seen block: \(.*\).*/\1/p' | xargs)
-    diff=$(($seen_block - $current_block))
-    waited=0
-    $echo "${INFO}Wait for agent catch up on chain${NC}"
-    while [ $diff -ne 0 ] || [ $seen_block -eq 0 ]; do
-        current_block=$(bw2 status  | sed -n -e 's/Current block: \(.*\).*/\1/p' | xargs)
-        seen_block=$(bw2 status  | sed -n -e 's/Seen block: \(.*\).*/\1/p' | xargs)
-        if [ $seen_block -ne 0 ]; then
-            diff=$(($seen_block - $current_block))
-            pct=$(bc <<< "scale=2; 100*$current_block/$seen_block")
-            human_waited=$(date -u -d @${waited} +"%T")
-            printf "\r$current_block/$seen_block ($pct%% done). Waited $human_waited so far..."
-        else
-            printf "\rWaiting for peers to send blocks..."
-        fi
-        sleep 1
-        waited=$(($waited + 1))
-    done
-    $echo "${OK}Installed BOSSWAVE${NC}"
+    configure_entities
 
     if $CONFIGURE_NAMESPACE ; then
-        # configure BW2_AGENT
-        $echo "${INFO}Rewriting BW2_AGENT to 172.17.0.1:28589 for docker${NC}"
-        $sh_c "sed -i -e 's/ListenOn=127.0.0.1:28589/ListenOn=172.17.0.1:28589/' /etc/bw2/bw2.ini"
-        export BW2_AGENT="172.17.0.1:28589"
-        $echo "${INFO}restarting...${NC}"
-        $sh_c "$reload_bw2"
-        sleep 10
-    else
-        export BW2_AGENT="127.0.0.1:28589"
-    fi
-    $echo "${OK}Reconfigured BOSSWAVE${NC}"
-
-    # configure BW2_DEFAULT_ENTITY
-    bw2 inspect $BW2_DEFAULT_ENTITY > /dev/null 2>&1
-    if [ -z "$BW2_DEFAULT_ENTITY" ] || [ $? -ne 0 ]; then
-        if [ ! -f defaultentity.ent ]; then
-            $echo "${INFO}Could not find BW2_DEFAULT_ENTITY. Creating defaultentity.ent${NC}"
-            bw2 mke -o defaultentity.ent -e 100y -m "Administrative key" -n
-            # sleep_til_valid defaultentity.ent
-        fi
-        export BW2_DEFAULT_ENTITY="$(pwd)/defaultentity.ent"
-    fi
-
-    bw2 inspect $BW2_DEFAULT_BANKROLL > /dev/null 2>&1
-    if [ -z "$BW2_DEFAULT_BANKROLL" ] || [ $? -ne 0 ]; then
-        $echo "${INFO}Could not find BW2_DEFAULT_BANKROLL. Setting it to ${BW2_DEFAULT_ENTITY} ${NC}"
-        export BW2_DEFAULT_BANKROLL="$BW2_DEFAULT_ENTITY"
-    fi
-    git add defaultentity.ent
-    git commit -m 'Added default entity and bankroll'
-
-    # get the monEH
-    address=$(bw2 i $BW2_DEFAULT_BANKROLL | sed -n 's/.* 0 (\(0x.*\)) .*/\1/p')
-    funds=$(bw2 i $BW2_DEFAULT_BANKROLL | sed -n 's/.* 0 (0x.*) \([0-9]*\).* .*/\1/p')
-    if [[ "$funds" -lt 500 ]] ; then
-        $echo "${INFO}You only have $funds Ξ, but we recommend 500 Ξ. ${NC}"
-        $echo "${INFO}Ask someone to send $((500 - $funds)) Ξ to address $address . We'll wait${NC}"
-        confirmY "${PROMPT}Do you have the funds? If you don't, we will try our best, but some commands may fail. Continue? [Y/n]${NC}"
-        if [ $? -ne 0 ]; then exit ;fi
-    fi
-
-    # publish default entity
-    bw2 i $BW2_DEFAULT_ENTITY -p
-    $echo "${OK}Now have \$BW2_DEFAULT_ENTITY, \$BW2_DEFAULT_BANKROLL${NC}"
-
-    if $CONFIGURE_NAMESPACE ; then
-        $echo "${INFO}Setting up namespace${NC}"
-        # setup namespace entity
-
-        # copy over existing entity if configured
-        if [ ! -z "$NAMESPACE_ENTITY" ]; then
-            $sh_c "cp $NAMESPACE_ENTITY namespace.ent"
-        fi
-
-        # setup entity if it doesn't exist
-        if [ ! -f "namespace.ent" ]; then
-            $echo "${INFO}Creating a namespace entity${NC}"
-            bw2 mke -o namespace.ent -e 100y -m "Namespace entity"
-            sleep_til_valid namespace.ent
-        fi
-
-        set -e
-        nsvk=$(bw2 i namespace.ent | sed -n 's/.*Entity VK: \(.*\).*/\1/p')
-
-        # setup alias if its null
-        export EXISTING_ALIAS=$(bw2 i namespace.ent | sed -n 's/.*Alias: \(.*\).*/\1/p')
-        if [ "$EXISTING_ALIAS" != "$NAMESPACE_ALIAS" ] && [ ! -z "$NAMESPACE_ALIAS" ] && [ ! -z "$nsvk" ] ; then
-            $echo "${INFO}Creating namespace alias${NC}"
-            bw2 mkalias --long "$NAMESPACE_ALIAS" --b64 "$nsvk"
-        else
-            if [ -z "$NAMESPACE_ALIAS" ]; then
-                $echo "${ERROR}No alias configured, using VK ${NC}"
-                export NAMESPACE_ALIAS=$nsvk
-            fi
-            $echo "${OK}Already have alias $NAMESPACE_ALIAS${NC}"
-        fi
-        set +e
-
-        # confirm admin privileges on namespace for defaultentity.ent
-        bw2 bc -u "$NAMESPACE_ALIAS/*"
-        if [ $? -ne 0 ]; then
-            $echo "${INFO}Make DOT on $NAMESPACE_ALIAS/*${NC}"
-            set -e
-            bw2 mkdot -f namespace.ent -t $BW2_DEFAULT_ENTITY -u "$NAMESPACE_ALIAS/*" -m "Admin access to $NAMESPACE_ALIAS" --ttl 10
-            set +e
-        fi
-
-        # setup designated router
-        if [ ! -z "$DESIGNATED_ROUTER_VK" ]; then
-            $echo "${INFO}Now we need to set up the designated router (DR) for this namespace. Ask someone who runs a DR to run the following:${NC}"
-            $echo "${INFO}bw2 mkdroffer --dr /etc/bw2/router.ent --ns $NAMESPACE_ALIAS${NC}"
-            #$echo "${INFO}Now type in the VK of the DR router entity (obtained by bw2 lsdro --ns $NAMESPACE_ALIAS, or by asking)${NC}"
-            bw2 adro --dr $DESIGNATED_ROUTER_VK --ns namespace.ent
-        fi
-        $echo "${GO}Namespace configured${NC}"
-        git add namespace.ent
-        git commit -m 'Configured namespace'
+        configure_namespace
     fi
 
     # install go
     if $INSTALL_GO ; then
-        $echo "${YELLOW}Installing Go${NC}"
-        $sh_c "curl -O https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz"
-        $sh_c "tar -C /usr/local -xzf go1.9.linux-amd64.tar.gz"
-        export PATH=/usr/local/go/bin:$PATH
-        export PATH=$(go env GOPATH):$PATH
-
-        $echo "${OK}Installed go${NC}"
+        install_go
     fi
 
     if $INSTALL_SPAWNPOINT_SERVER ; then
-        # create spawnpoint entity
-        $echo "${YELLOW}Installing spawnpoint${NC}"
-
-        # copy spawnd entity over if configured
-        if [ ! -z "$SPAWND_ENTITY" ]; then
-            $sh_c "cp $SPAWND_ENTITY spawnpoint.ent"
-        fi
-
-        # or create it if it doesn't exist
-        if [ ! -f spawnpoint.ent ]; then
-            set -e
-            $echo "${INFO}Could not find spawnpoint.ent. Creating...${NC}"
-            bw2 mke -o spawnpoint.ent -e 100y -m "Spawnpoint entity"
-            sleep_til_valid spawnpoint.ent
-            $echo "${INFO}Updating permissions for spawnpoint.ent${NC}"
-            bw2 mkdot -f namespace.ent -t spawnpoint.ent -u "$NAMESPACE_ALIAS/sp/*" -m "Spawnpoint access"
-            set +e
-        fi
-
-        # install spawnpoint server
-        $sh_c "cp spawnpoint.ent /etc/spawnd/spawnpooint.ent"
-        export SPAWND_INSTALLER_ENTITY=spawnpoint.ent
-        export SPAWND_INSTALLER_PATH="$NAMESPACE_ALIAS/sp"
-        export SPAWND_INSTALLER_MEM_ALLOC="$SPAWND_MEM_ALLOC"
-        export SPAWND_INSTALLER_CPU_SHARES="$SPAWND_CPU_SHARES"
-        $sh_c $(curl get.bw2.io/spawnpoint | bash)
-        git add spawnpoint.ent
-        git commit -m 'Configured spawnpoint'
+        install_spawnd
     fi
 
     if $INSTALL_SPAWNPOINT_CLIENT ; then
