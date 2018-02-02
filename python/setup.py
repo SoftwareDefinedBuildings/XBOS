0a6dcd94392fe92eeaeef45b9b39ea744346db1c
Gabe Fierro <gtfierro225@gmail.com>
2018-02-01 18:53:49 -0800

    bump to 0.0.24

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 print find_packages()
 setup(name='xbos',
-      version='0.0.23',
+      version='0.0.24',
       description='Aggregate wrapper for XBOS services and devices',
       url='https://github.com/SoftwareDefinedBuildings/XBOS',
       author='Gabe Fierro',
