3d921121e8af124d1d938a2c6ddde5aec5ccc6a4
Brandon Berookhim <brandonberookhim@gmail.com>
2018-07-09 17:59:07 -0700

    scaffolding for sorting zones

--- dashboards/xbos-dash/assets/js/DR/DR.js
+++ dashboards/xbos-dash/assets/js/DR/DR.js
@@ -8,40 +8,39 @@ $(document).ready(function() {
 			$("#switch-zone").removeClass("black-text");
 			$("#zone-config").hide();
 			$("#bldng-config").show();
-			// $("#lever").prop("checked", false);
 		} else {
 			$("#switch-zone").addClass("black-text");
 			$("#switch-bldng").removeClass("black-text");
 			$("#bldng-config").hide();
 			$("#zone-config").show();
 			$("#avg-sim-btn").addClass("scale-in");
-			// $("#lever").prop("checked", true);
 		}
 		checked = !checked;
 	}
 
 	$("#my-div").click(function(event) { event.stopImmediatePropagation(); $("#checkbox").prop("checked", checked); });
 	$("#label").click(function(event) { event.stopImmediatePropagation(); $("#checkbox").prop("checked", checked); });
-	$("#switch-bldng").click(function(event) { event.stopImmediatePropagation(); $("#checkbox").prop("checked", checked); });
+	$("#switch-bldng").click(function(event) { event.stopImmediatePropagation(); checked = true; mySwitch(); $("#checkbox").prop("checked", checked); });
 	$("#lever").click(function(event) { event.stopImmediatePropagation(); mySwitch(); });
 	$("#checkbox").click(function(event) { event.stopImmediatePropagation(); $("#checkbox").prop("checked", checked); });
-	$("#switch-zone").click(function(event) { event.stopImmediatePropagation(); $("#checkbox").prop("checked", checked); });
+	$("#switch-zone").click(function(event) { event.stopImmediatePropagation(); checked = false; mySwitch(); $("#checkbox").prop("checked", checked); });
 
 	let l = 18;
 	var s = "";
 	for (var i = 1; i <= l; i += 1) {
 		if (i % 2 == 1) { s += "<div class='row valign-wrapper'>"; }
-		s += "<div class='col s7 z-depth-1 hoverable' style='padding: 18px 30px; border-radius: 2px;'>";
-		s += "<h5 class='center-align' style='margin: 0;' id='z" + i + "banner'>Zone " + i + "</h5>";
+		s += "<div id='z" + i + "card' class='col s12 zone-card z-depth-1 hoverable' style='padding: 18px 30px; border-radius: 2px;'>";
+		s += "<h6 id='z" + i + "note' style='margin: 0;'></h6>";
+		s += "<h5 class='center-align' style='margin-bottom: 0;' id='z" + i + "banner'>Zone " + i + "</h5>";
 		s += "<p class='range-field'><input id='z" + i + "range' class='simrange center-align' type='range' min='0' max='1' value='0.50' step='0.01'/></p>";
 		s += "<div style='display: flex; justify-content: space-between;'>";
 		s += "<h5 id='z" + i + "date' class='grey-text' style='margin-top: 0;'>Historical</h5>";
-		s += "<h5 style='margin-top: 0;'>Simulated</h5>";
+		s += "<h5 style='margin-top: 0;'>Simulation</h5>";
 		s += "</div>";
 		s += "<div style='display: flex;'>";
 		s += "<h5 style='width: 25%; margin-top: 0;' id='z" + i + "hislam' class='hislam grey-text left-align'>____</h5>";
 		s += "<h5 class='center-align' style='width: 50%; margin-top: 0;'>λ</h5>";
-		s += "<h5 class='right-align' style='width: 25%; margin-top: 0;' id='z" + i + "simlam'>0.50</h5>";
+		s += "<h5 class='right-align' style='width: 25%; margin-top: 0;' id='z" + i + "simlam'>____</h5>";
 		s += "</div>";
 		s += "<div style='display: flex;'>";
 		s += "<h5 style='width: 25%; margin-top: 0;' id='z" + i + "dis' class='hisdis grey-text left-align'>____</h5>";
@@ -66,40 +65,15 @@ $(document).ready(function() {
 				s += "<div class='col s1'></div>";
 			}
 		} else {
-			if (i % 2 == 0) { s += "</div>"; s += "<div class='row'></div><div class='row'></div>"; }
+			if (i % 2 == 0) { s += "</div>"; s += "<div class='row'></div>"; }
 		}
 	}
 	$("#zone-config").append(s);
-
-
-	// $("#checkbox").click(function() { console.log("checkbox"); });
-	// $("#lever").click(function() { console.log("lever"); });
-	// $("#switch-zone").click(function() { console.log("switch"); });
-
-	$("#lever").click(mySwitch);
-
-	// $("#switch-zone").('click', function (event) {
-	// 	event.stopPropagation();
-	// 	$("#lever").prop("checked", true);
-	// 	$("#switch-zone").addClass("black-text");
-	// 	$("#switch-bldng").removeClass("black-text");
-	// 	$("#bldng-config").hide();
-	// 	$("#zone-config").show();
-	// 	// if ($(event.target).is($("#switch-zone"))) {
-	// 	// 	alert('clicked');
-	// 	// }
-	// });
-
-	// $("#switch-zone").click(function() {
-	// 	// checked = false;
-	// 	mySwitch();
-	// });
 
 	function myFix(x) {
 		if (x > 1) {
 			return x;
 		}
-		// x = x.toString();
 		if (x == 0) {
 			return "0.0";
 		} else if (x == 1) {
@@ -117,6 +91,7 @@ $(document).ready(function() {
 		this.oninput = function() {
 			if (b) { $("#sim-btn").addClass("scale-in"); b = false; };
 			x.html(myFix(this.value));
+			clearBldng();
 		};
 	});
 
@@ -134,8 +109,8 @@ $(document).ready(function() {
 	$(".simrange").each(function() {
 		var x = $("#" + this.id.replace("range", "simlam"));
 		this.oninput = function() {
-			// x.html("λ: " + myFix(this.value));
 			x.html(myFix(this.value));
+			$("#" + this.id.replace("range", "card")).addClass("grey").addClass("lighten-4");
 			setLamAvg();
 			clearZone(this.id.replace("range", ""));
 			clearSummary();
@@ -186,7 +161,15 @@ $(document).ready(function() {
 		$(k).html(myFix(x.toFixed(2)));
 	}
 
-	// setSummaryVals();
+	function clearBldng() {
+		$("#historic-lam").html("_____");
+		$("#historic-dis").html("_____");
+		$("#historic-money-savings").html("_____");
+		$("#historic-energy-savings").html("_____");
+		$("#sim-dis").html("_____");
+		$("#sim-money-savings").html("_____");
+		$("#sim-energy-savings").html("_____");
+	}
 
 });
 				
