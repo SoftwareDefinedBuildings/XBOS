a01e9ff4c101d97cd1fdb315c0abe9aea5afa793
callieclark <crclark@berkeley.edu>
2019-01-31 09:25:26 -0800

    Delete __init__.py

--- apps/Data_analytics/__init__.py
+++
@@ -1,5 +0,0 @@
-from Energy_Analytics.Import_Data import *
-from Energy_Analytics.Clean_Data import *
-from Energy_Analytics.Preprocess_Data import *
-from Energy_Analytics.Model_Data import *
-from Energy_Analytics.Wrapper import *
