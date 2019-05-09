b9e999711139eb841a9f3537348ea409d9bbc758
Gabe Fierro <gtfierro225@gmail.com>
2019-05-09 14:46:11 -0700

    remove old files

--- driver/errors.go
+++
@@ -1,8 +0,0 @@
-package driver
-
-import (
-	"github.com/pkg/errors"
-)
-
-var ErrUnknownSignal = errors.New("Unknown signal")
-var ErrUnknownSlot = errors.New("Unknown slot")
--- xbos/container/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-cd .. ; go build -o xbos ; cd -
-cp ../xbos .
-docker build --rm -t gtfierro/xbosclitest .
