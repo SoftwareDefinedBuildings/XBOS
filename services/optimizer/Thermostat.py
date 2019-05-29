91df8bf17ca6dda2a5eddbf92f0974dee0b53a10
Daniel Lengyel <daniel.lengyel@berkeley.edu>
2019-05-28 17:37:21 -0700

    updated tstat so it does not throw error when not getting data

--- services/optimizer/Thermostat.py
+++ services/optimizer/Thermostat.py
@@ -8,7 +8,7 @@ class Tstat:
     STANDARD_VAR = 1
     STANDARD_UNIT = "F"
 
-    def __init__(self, building, zone, temperature, last_temperature=None):
+    def __init__(self, building, zone, temperature, last_temperature=None, suppress_not_enough_data_error=True):
         self.temperature = temperature
         self.last_temperature = last_temperature
         self.indoor_temperature_prediction_stub = xsg.get_indoor_temperature_prediction_stub()
@@ -20,7 +20,11 @@ class Tstat:
                                                                               zone,
                                                                               action)
             except:
-                print("Tstat for building: '{0}' and zone: '{1}' did not receive error data from "
+                if not suppress_not_enough_data_error:
+                    raise  Exception("ERROR: Tstat for building: '{0}' and zone: '{1}' did not receive error data from "
+                      "indoor_temperature_prediction microservice for action: '{2}'.")
+
+                print("WARNING: Tstat for building: '{0}' and zone: '{1}' did not receive error data from "
                       "indoor_temperature_prediction microservice for action: '{2}' and is now using STANDARD error.".format(building, zone, action))
                 mean, var, unit = Tstat.STANDARD_MEAN, Tstat.STANDARD_VAR, Tstat.STANDARD_UNIT
 
